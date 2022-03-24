63c98c1fc5ee76771c2af10dc4de7d5817725d33
Abhinav Anand <abhi9vanand@gmail.com>
2022-03-24 13:29:45 +0530

    screenshots added

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { NativeModules, Button } from 'react-native';
+import React, {Component} from 'react';
+import { NativeModules, Button, View } from 'react-native';
 
 const { CalendarModule } = NativeModules;
 
 const NewModuleButton = () => {
   const onPress = () => {
-    CalendarModule.createCalendarEvent('Abhinav', 'New Delhi');
+    CalendarModule.createCalendarEvent('Abhinav Anand', 'New Delhi');
     console.log('We will invoke the native module here!');
   };
 
   return (
+    <View style={{flex:1, justifyContent:'center', backgroundColor:'#FBD6D2'}}>
     <Button
       title="Click to invoke your native module!"
-      color="#841584"
+      color="#A63EC5"
       onPress={onPress}
     />
+    </View>
   );
 };
 
